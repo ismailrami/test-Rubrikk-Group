8fe090f95c1215b0607c2028a5f2050890c57e9a
rami ismail <ismail.ismailrami@gmail.com>
2016-11-06 17:37:53 +0100

    first commit

---
+++ test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> items = new List<double>();
+            double number = 0;
+            Console.WriteLine("insert number");
+            while (double.TryParse(Console.ReadLine(), out number))
+            {
+                
+                items.Add(number);
+            }
+            RandomNumber rand = new RandomNumber();
+            rand.GetRandomNumber(items);
+            Console.Write(rand.GetRandomNumber(items));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ test/RandomNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class RandomNumber
+    {
+        public int GetRandomNumber(List<double> items)
+        {
+            Random r = new Random();
+            double rand = r.NextDouble();
+
+            double probability = 0.0;
+            double sum = 0.0;
+            int selectedElement = 0;
+            for (int i = 0; i < items.Count; i++)
+                sum += items[i];
+            for (int i = 0; i < items.Count; i++)
+            {
+                probability += items[i]/sum;
+                if (rand < probability)
+                {
+                    selectedElement = i;
+                    break;
+                }
+            }
+            return selectedElement;
+        }
+    }
+}
